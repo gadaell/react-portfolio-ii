f7b4ee064071b5bbad17b59782d8c79724e4e60f
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-18 07:34:40 -0600

    restarted project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
+
+const App = () => (
+  <div>
+    <header className="header-img light-txt">
+      <h1 class="py-2 px-3">A'Darius L. Ellis</h1>
+    </header>
+    <PortfolioContainer />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="my-5 background-img">
+      <div className="container">
+        <h1 id="about">About Me</h1>
+        <div className="row justify-content-center">
+          <a href="https://github.com/gadaell"></a>
+          <p className="m-4 about-txt">
+            Salutations, my name is A'Darius Ellis, but everyone knows me as
+            A'D. I attended the University of Memphis for my undergraduate
+            career, and accomplished on receiving my Bachelor of Science in
+            Health Science. I have been in the supply chain industry for 5
+            years. I started my career at ABF as a dock worker, and eventually
+            worked my way up to Operation Manager Asst. I decided to pursue more
+            educated and decided to attend the coding Bootcamp program at
+            Vanderbilt University provided by their partner, Trilogy. I am
+            currently engaged to my beautiful fiancee, Amber, and we have a 1
+            year goldendoodle named Shilo. Thank you for taking your time out of
+            your hands to review my portfolio.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
